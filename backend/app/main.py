6df490cf73d36e0e5ab8e249f77b60f3cd80011f
Akshay Sharma <akshaybhardwaj411@gmail.com>
2026-09-09 11:07:39 +0530

    Include customer_360 router in main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.api.customer_360 import router as customer_360_router
 from app.api.customers import router as customers_router
 from app.api.organizations import router as organizations_router
 
@@ -12,6 +13,7 @@ app = FastAPI(
 
 app.include_router(organizations_router)
 app.include_router(customers_router)
+app.include_router(customer_360_router)
 
 
 @app.get("/")
